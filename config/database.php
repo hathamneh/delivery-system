d1b68ef27ba2e5bb9e1135f552b4aec52b0cbee4
haithamAth <himoath@gmail.com>
2018-07-13 17:19:01 +0300

    add secondary name as env var

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => "kangaroo_migration_db",
+            'database' => env('DB_SECONDARY_NAME', 'kangaroo_migration_db'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),

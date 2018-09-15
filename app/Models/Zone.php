83d9ba77d8f1508401c74ae3701954a79b8f8a63
haithamAth <himoath@gmail.com>
2018-09-15 16:30:52 +0300

    fix custom zones and addresses

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -38,6 +38,7 @@ class Zone extends Model
         static::updating(function (Zone $zone) {
             if (!$zone instanceof CustomZone) {
                 logger(DB::table('custom_zones')->where('zone_id', $zone->id)->update(['name' => $zone->name]));
+                logger(DB::getQueryLog());
             }
         });
 

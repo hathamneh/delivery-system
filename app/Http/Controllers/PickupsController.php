f87263f5a4de4724aa6645eafd80d7b83eb32957
haithamath <himoath@gmail.com>
2019-03-14 22:34:38 +0200

    Pickups page for courier shows all if lifetime selected

--- app/Http/Controllers/PickupsController.php
+++ app/Http/Controllers/PickupsController.php
@@ -38,10 +38,7 @@ class PickupsController extends Controller
 
         $pickups   = Pickup::latest('available_time_start');
         $startDate = $endDate = false;
-        if (Auth::user()->isCourier()) {
-            $pickups = Pickup::latest('available_time_start');
-            $pickups->today()->where('courier_id', Auth::user()->courier->id);
-        } elseif ($request->has('start') && $request->has('end')) {
+        if ($request->has('start') && $request->has('end')) {
             $startDate = Carbon::createFromTimestamp($request->get('start'));
             $endDate   = Carbon::createFromTimestamp($request->get('end'));
             $pickups->whereDate('available_time_start', '>=', $startDate)->whereDate('available_time_end', '<=', $endDate);

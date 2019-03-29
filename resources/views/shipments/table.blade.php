8d8b1d09e0e2100e65f2502fa181910174503c37
haithamath <himoath@gmail.com>
2019-03-29 18:35:17 +0300

    shipments table fix

--- resources/views/shipments/table.blade.php
+++ resources/views/shipments/table.blade.php
@@ -1,7 +1,7 @@
 @if(auth()->user()->isCourier())
     @include('shipments.table.couriers')
 @else
-    @if(in_array('returned',$applied['types'])))
+    @if(isset($applied) && isset($applied['types']) && in_array('returned', $applied['types'])))
         @include('shipments.table.returned')
     @else
         @include('shipments.table.default')
